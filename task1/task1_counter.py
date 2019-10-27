388fe204ead36020de2d293f03b030efef75bc9b
Alex_Rez <reznikova.am@gmail.com>
2019-10-28 01:12:56 +0300

    изменила цикл на генератор

--- task1/task1_counter.py
+++ task1/task1_counter.py
@@ -1,17 +1,14 @@
 def SymbolCounter(files):
-    letter_counter = dict()
+    let_count = dict()
     all_letters = 0
     for file in files:
-        f = open(file, 'r', encoding='utf-8')
+        with open(file, 'r', encoding='utf-8') as f:
+            dict_gen = (let_count.update({let: let_count.get(let, 0) + 1}) for line in f.readlines() for let in line.lower() if let.isalpha())
+            list(dict_gen)
 
-        for line in f.readlines():
-            all_letters += len(line) - 1
-            for letter in line.lower():
-                if(letter.isalpha()):
-                    letter_counter[letter] = letter_counter.setdefault(letter, 0) + 1
-
-    letter_freq = {letter: letter_counter[letter] / all_letters for letter in letter_counter}
-    sorted_freq = sorted(letter_freq.items(), key=lambda key_value: key_value[1], reverse=True)
+    all_letters = sum(n for n in let_count.values())
+    let_count = {let: let_count[let] / all_letters for let in let_count}
+    sorted_freq = sorted(let_count.items(), key=lambda key_value: key_value[1], reverse=True)
     return sorted_freq
 
 # files_list = ['voyna-i-mir-tom-1.txt', 'voyna-i-mir-tom-2.txt', 'voyna-i-mir-tom-3.txt', 'voyna-i-mir-tom-4.txt']
